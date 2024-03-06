312c2110555fee816f768f4ca4ca53b003f66db5
Rafael Seles de Souza <rafa30262317@gmail.com>
2024-03-06 14:11:08 -0300

    inciando projeto, criando os componenetes Navegacao e Atracao

---
+++ src/components/Navegacao/Navegacao.jsx
@@ -0,0 +1,26 @@
+import './Navegacao.css'
+
+
+function Navegacao() {
+
+    return (
+        <div className="barra-navegacao">
+            <div className="logo-zoo">
+                <h1>ZOOLÓGICO</h1>
+            </div>
+
+            <div className='navbar-opcoes'>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Os animais</li>
+                    <li>Fotos</li>
+                    <li>Depoimentos</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+
+        </div>
+    );
+}
+
+export default Navegacao
